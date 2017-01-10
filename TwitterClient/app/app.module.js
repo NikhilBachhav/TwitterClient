6a4309e3b6e26f964a274bb0f775fdc49bbae0cb
Nikhil Bachhav <nabachhav@gmail.com>
2017-01-10 11:11:25 -0800

    Add files via upload

---
+++ TwitterClient/app/app.module.js
@@ -0,0 +1,14 @@
+'use strict';
+// Declare app level module which depends on views, and components
+angular.module('twitterApp', [
+    'ngAria',
+    'ui.router',
+    'controllers',
+    'directives',
+    'services',
+    'ngTable',
+    'ngResource',
+    'ngSanitize',
+    'pascalprecht.translate'
+]);
+
---
+++ TwitterClient/app/controller/controllers.js
@@ -0,0 +1,66 @@
+//inject the twitterService into the controller
+app.controller('TwitterController', function($scope,$q, twitterService) {
+
+   $scope.tweets=[]; //array of tweets
+
+    $scope.submitTweet = function() {
+        twitterService.saveTweet($scope.tweet.message).then(function(result) {
+            $scope.tweet.message = '';
+            $scope.refreshTimeline();
+        });
+    }
+
+    twitterService.initialize();
+
+    //using the OAuth authorization result get the latest 20 tweets from twitter for the user
+    $scope.refreshTimeline = function(maxId) {
+        twitterService.getLatestTweets(maxId).then(function(data) {
+            if(!maxId) {
+                $scope.tweets = [];
+            }
+            $scope.tweets = $scope.tweets.concat(data);
+        },function(){
+            $scope.rateLimitError = true;
+        });
+    }
+
+
+    //when the user clicks the connect twitter button, the popup authorization window opens
+    $scope.connectButton = function() {
+        twitterService.connectTwitter().then(function() {
+            if (twitterService.isReady()) {
+                //if the authorization is successful, hide the connect button and display the tweets
+                $('#connectButton').fadeOut(function(){
+                    $('#getTimelineButton, #signOut').fadeIn();
+                    $('#addTweet, #signOut').fadeIn();
+                    $scope.refreshTimeline();
+                    $scope.connectedTwitter = true;
+                });
+            } else {
+
+            }
+        });
+    }
+
+    //sign out clears the OAuth cache, the user will have to reauthenticate when returning
+    $scope.signOut = function() {
+        twitterService.clearCache();
+        $scope.tweets.length = 0;
+        $('#addTweet, #signOut').fadeOut();
+        $('#getTimelineButton, #signOut').fadeOut(function(){
+            $('#connectButton').fadeIn();
+            $scope.$apply(function(){$scope.connectedTwitter=false})
+        });
+        $scope.rateLimitError = false;
+    }
+
+    //if the user is a returning user, hide the sign in button and display the tweets
+    if (twitterService.isReady()) {
+        $('#connectButton').hide();
+        $('#getTimelineButton, #signOut').show();
+        $('#addTweet, #signOut').fadeIn();
+        $scope.connectedTwitter = true;
+        $scope.refreshTimeline();
+    }
+
+});
